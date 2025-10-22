d50354a62d54c192164fb8eb38197a786dc67352
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-10-22 16:10:58 +0530

    using axios to fetch api

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,20 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:7777/login", {
+        emailId,
+        password,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="flex justify-center my-10">
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
@@ -27,7 +38,9 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="btn btn-neutral mt-4">Login</button>
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+          Login
+        </button>
       </fieldset>
     </div>
   );
